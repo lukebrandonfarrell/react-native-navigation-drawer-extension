17049f5d3e12e9e16874bf51ec89a147c1a075dc
Rodrigo Vieira <rodriigovieira@gmail.com>
2020-11-11 15:47:55 -0300

    adding types back again

--- lib/RNNDrawer.d.ts
+++ lib/RNNDrawer.d.ts
@@ -4,14 +4,68 @@
  */
 import * as React from 'react';
 import { Layout } from 'react-native-navigation';
+
+type DirectionType = "left" | "right" | "top" | "bottom";
+
+declare interface RNNDrawerOptions {
+    /**
+     * Id of parent component of the drawer.
+     * This field is necessary in order to be able
+     * to push screens inside the drawer
+     */
+    parentComponentId: string;
+
+    /**
+     * Time in milliseconds to execute the drawer opening animation
+     */
+    animationOpenTime?: number;
+
+    /**
+     * Time in milliseconds to execute the drawer closing animation
+     */
+    animationCloseTime?: number;
+
+    /**
+     * Direction to open the collage,
+     * one of: ["left", "right", "top", "bottom"]
+     */
+    direction?: DirectionType;
+
+    /**
+     * Whether the drawer be dismissed when a click is registered outside
+     */
+    dismissWhenTouchOutside?: boolean;
+
+    /**
+     * Opacity of the screen outside the drawer
+     */
+    fadeOpacity?: number;
+
+    /**
+     * Width of drawer in relation to the screen (0 to 1)
+     */
+    drawerScreenWidth?: number;
+
+    /**
+     * Height of drawer in relation to the screen (0 to 1)
+     */
+    drawerScreenHeight?: number;
+}
+
 declare class RNNDrawer {
+    /**
+     * Generates the drawer component to
+     * be used with react-native-navigation
+     * 
+     * @param component
+     */
     static create(Component: React.ComponentType): any;
     /**
      * Shows a drawer component
      *
      * @param layout
      */
-    static showDrawer(layout: Layout): void;
+    static showDrawer(layout: Layout<RNNDrawerOptions>): void;
     /**
      * Dismiss the drawer component
      */
