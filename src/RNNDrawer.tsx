7c731919e3dec873fd8133ef21485578bc2598c4
Rodrigo Vieira <rodriigovieira@gmail.com>
2020-11-12 03:59:59 -0300

    adding sensitivity to swipe and fixes on ios

--- src/RNNDrawer.tsx
+++ src/RNNDrawer.tsx
@@ -25,50 +25,50 @@ import { listen, dispatch } from './events';
 const MaxWidthOnLandscapeMode = 300;
 
 declare interface RNNDrawerOptions {
-    /**
-     * Id of parent component of the drawer.
-     * This field is necessary in order to be able
-     * to push screens inside the drawer
-     */
-    parentComponentId: string;
+  /**
+   * Id of parent component of the drawer.
+   * This field is necessary in order to be able
+   * to push screens inside the drawer
+   */
+  parentComponentId: string;
 
-    /**
-     * Direction to open the collage,
-     * one of: ["left", "right", "top", "bottom"]
-     * If not provided, drawer  might have
-     * a weird effect when closing
-     */
-    direction: DirectionType;
+  /**
+   * Direction to open the collage,
+   * one of: ["left", "right", "top", "bottom"]
+   * If not provided, drawer  might have
+   * a weird effect when closing
+   */
+  direction: DirectionType;
 
-    /**
-     * Time in milliseconds to execute the drawer opening animation
-     */
-    animationOpenTime?: number;
+  /**
+   * Time in milliseconds to execute the drawer opening animation
+   */
+  animationOpenTime?: number;
 
-    /**
-     * Time in milliseconds to execute the drawer closing animation
-     */
-    animationCloseTime?: number;
+  /**
+   * Time in milliseconds to execute the drawer closing animation
+   */
+  animationCloseTime?: number;
 
-    /**
-     * Whether the drawer be dismissed when a click is registered outside
-     */
-    dismissWhenTouchOutside?: boolean;
+  /**
+   * Whether the drawer be dismissed when a click is registered outside
+   */
+  dismissWhenTouchOutside?: boolean;
 
-    /**
-     * Opacity of the screen outside the drawer
-     */
-    fadeOpacity?: number;
+  /**
+   * Opacity of the screen outside the drawer
+   */
+  fadeOpacity?: number;
 
-    /**
-     * Width of drawer in relation to the screen (0 to 1)
-     */
-    drawerScreenWidth?: number;
+  /**
+   * Width of drawer in relation to the screen (0 to 1)
+   */
+  drawerScreenWidth?: number;
 
-    /**
-     * Height of drawer in relation to the screen (0 to 1)
-     */
-    drawerScreenHeight?: number;
+  /**
+   * Height of drawer in relation to the screen (0 to 1)
+   */
+  drawerScreenHeight?: number;
 }
 
 
@@ -130,12 +130,12 @@ interface SwipeMoveInterface {
 }
 
 class RNNDrawer {
-   /**
-   * Generates the drawer component to
-   * be used with react-native-navigation
-   * 
-   * @param component
-   */
+  /**
+  * Generates the drawer component to
+  * be used with react-native-navigation
+  * 
+  * @param component
+  */
   static create(Component: React.ComponentType): any {
     class WrappedDrawer extends React.Component<IProps, IState> {
       private readonly screenWidth: number;
@@ -186,7 +186,11 @@ class RNNDrawer {
           onMoveShouldSetPanResponder: (
             _evt: GestureResponderEvent,
             _gestureState: PanResponderGestureState,
-          ) => true,
+          ) => {
+            const { dx } = _gestureState;
+
+            return Math.abs(dx) > 5;
+          },
           onMoveShouldSetPanResponderCapture: (
             _evt: GestureResponderEvent,
             _gestureState: PanResponderGestureState,
@@ -373,7 +377,7 @@ class RNNDrawer {
           if (this.props.direction === "right") {
             // Calculates the position of the drawer from the left side of the screen
             const alignedMovementValue = window.width - this.drawerWidth;
-            
+
             this.state.sideMenuOpenValue.setValue(alignedMovementValue);
           }
         })
